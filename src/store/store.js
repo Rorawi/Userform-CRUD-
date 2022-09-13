7247abeb5e12a66a1b5c0c0742e0b1829f19824a
Winifred <winifredasante15@gmail.com>
2022-09-13 11:46:36 -0400

    dispatch

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import userReducer  from "../reducer/userReducer";
 import thunk from 'redux-thunk'
+
 const Store = createStore(userReducer, applyMiddleware(thunk));
 
 
